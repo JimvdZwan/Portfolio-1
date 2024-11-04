5d79d3e7ff0d08d76f4d9e55be844f709f233ca9
Jim van der Zwan <jim.van.der.zwan@windesheim.nl>
2024-11-04 16:14:19 +0100

    klaar

--- views/project.view.php
+++ views/project.view.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Thisis a sample webpage demonstratingthe useof meta tags in HTML5."></metaname>
     <meta name="keywords" content="HTML5, meta tags, web development">
-    <meta name="author"content="Jane Doe">
+    <meta name="author"content="Jim van der Zwan">
     <meta name="robots"content="index, follow">
     <link rel="stylesheet" href="../css/about.css">
     <link rel="stylesheet" href="../css/style.css">
